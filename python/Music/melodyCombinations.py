0ee7a5732a808c1774764d1af304feedc7829043
Palash <palash.kanti.kundu.90@gmail.com>
2021-06-04 18:42:13 +0530

    Commit melody generator first cut

--- python/Music/melodyCombinations.py
+++ python/Music/melodyCombinations.py
@@ -1,6 +1,7 @@
-from midiutil.MidiFile import MIDIFile
-import random
 import json
+import random
+
+from midiutil.MidiFile import MIDIFile
 
 
 def sums(length, total_sum):
@@ -12,13 +13,39 @@ def sums(length, total_sum):
                 yield (value,) + permutation
 
 
-inputFile = open('input.json')
-input = json.load(inputFile)
+def permutations(lst):
+    if len(lst) <= 1:
+        return [lst]  # [[X]]
+    l = []
+    for i in range(len(lst)):
+        m = lst[i]
+        remlst = lst[:i] + lst[i + 1:]
+        for p in permutations(remlst):
+            l.append([m] + p)
+    return l  # return at end of outer for loop
 
-choices = list(range(input['length']))
+
+# create your MIDI object
+mf = MIDIFile(1)  # only 1 track
+track = 0  # the only track
+
+time = 0  # start at the beginning
+mf.addTrackName(track, time, "Sample Track")
+mf.addTempo(track, time, 120)
+
+# add some notes
+channel = 0
+volume = 100
+
+time = 0
+
+inputFile = open('input.json')
+inp = json.load(inputFile)
+
+choices = list(range(inp['length']))
 random.shuffle(choices)
 
-numOfChords = len(input['chords'])
+numOfChords = len(inp['chords'])
 chordCombinations = []
 
 if numOfChords < 3:
@@ -31,11 +58,10 @@ for i in range(numOfChords):
                 if i != j and i != k and i != l and j != k and j != l and k != l:
                     chordCombinations.append(tuple([i, j, k, l]))
 
-
 for i in choices:
     combination = chordCombinations[i]
     for j in combination:
-        chord = input['chords'][combination[j]]
+        chord = inp['chords'][combination[j]]
 
         lowOctaveNotes = chord['lowOctaveNotes']
         middleOctaveNotes = chord['middleOctaveNotes']
@@ -43,9 +69,28 @@ for i in choices:
 
         totalNotes = lowOctaveNotes + middleOctaveNotes + highOctaveNotes
 
-        L = list(sums(len(totalNotes), input['numOfBar']*2))
-        print('total permutations:', len(L))
+        filteredNotes = []
+        for note in totalNotes:
+            if inp["noteRangeLow"] <= note <= inp["noteRangeHigh"]:
+                filteredNotes.append(note)
 
-        # First and last 10 of list
-        for i in L[:10] + L[-10:]:
-            print(i)
+        permutedNotes = permutations(filteredNotes)
+        random.shuffle(permutedNotes)
+        chosenNoteSequence = permutedNotes[0]
+
+        L = list(sums(len(filteredNotes), inp['numOfBar'] * 2))
+        random.shuffle(L)
+        chosenDurationSequence = L[0]
+
+        print(chosenNoteSequence, chosenDurationSequence)
+        for noteCounter in range(len(chosenNoteSequence)):
+            note = chosenNoteSequence[noteCounter]
+            duration = chosenDurationSequence[noteCounter]
+            if duration == 0:
+                continue
+            mf.addNote(track, channel, note, time, duration * 0.5, volume)
+            time = time + duration * 0.5
+
+# write it to disk
+with open("output.mid", 'wb') as outputFile:
+    mf.writeFile(outputFile)
